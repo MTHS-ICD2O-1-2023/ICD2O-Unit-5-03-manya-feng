6b992c15f5aa1ceee0a9424871debbb1e1d6c2e5
Manya Feng <159185413+manya-feng@users.noreply.github.com>
2024-04-29 18:47:42 +0000

    change

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Manya All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Manya
+// Created on: Apr 2024
 // This file contains the JS functions for index.html
+
+/**
+ * This function is check what kind of movie the users can watch.
+ */
+
+function myButtonClicked() {
+
+  const usersAges = parseInt(document.getElementById("types-movie").value)
+
+  if (usersAges >= 17) {
+    console.log("You can see an R movie alone.");
+  } else if(age >= 13) {
+    console.log("You can see a PG-13 movie alone.");
+  } else if(age >= 5) {
+    console.log("You can see a G or PG movie alone.");
+  } else {
+    console.log("Uh. You're too young for most things.");
+  }
+  console.log("Thanks for verifying your age!");
+  }
