0218acfdb84635f6318891fcaafe292004cb988e
Manya Feng <159185413+manya-feng@users.noreply.github.com>
2024-04-30 18:17:17 +0000

    change js

--- js/script.js
+++ js/script.js
@@ -3,27 +3,23 @@
 // Created by: Manya
 // Created on: Apr 2024
 // This file contains the JS functions for index.html
-
-/**
- * This function is check what kind of movie the users can watch.
- */
+"use strict"
 
 function myButtonClicked() {
-// input
-  const usersAges = parseInt(document.getElementById("types-movie").value)
+  const usersAges = parseInt(document.getElementById("usersage").value)
 
-  // process
-  if (usersAges == typesMovie) {
-    // output
-  if (usersAges >= 17) {
-    console.log("You can see an R movie alone.");
-  } else if(usersAges >= 13) {
-    console.log("You can see a PG-13 movie alone.");
-  } else if(usersAges >= 5) {
-    console.log("You can see a G or PG movie alone.");
-  } else {
-    console.log("Uh. You're too young for most things.");
+if (usersAges >= 17) {
+  document.getElementById("answer").innerHTML =
+    "You can see an R movie alone."
+} else if(usersAges >= 13) {
+  document.getElementById("answer").innerHTML =
+    "You can see a PG-13 movie alone."
+} else if(usersAges >= 5) {
+  document.getElementById("answer").innerHTML =
+    "You can see a G or PG movie alone."
+} else {
+  document.getElementById("answer").innerHTML =
+    "Uh. You're too young for most things."
   }
   console.log("Thanks for verifying your age!");
-  }
 }
